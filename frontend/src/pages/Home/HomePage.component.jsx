c7a6135b58d117cca10259b80ddc2df85bdd3562
Faiz Qureshi <faizqureshi111@gmail.com>
2020-06-24 16:36:37 -0600

    1. removed files i do not need 2. made basic folder structure 3. made basic home page component

---
+++ frontend/src/pages/Home/HomePage.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./HomePage.styles.css";
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <div className="dashboard-grid-container">
+        <div className="dashboard-grid-item">1</div>
+        <div className="dashboard-grid-item">2</div>
+        <div className="dashboard-grid-item">3</div>
+        <div className="dashboard-grid-item">4</div>
+        <div className="dashboard-grid-item">5</div>
+        <div className="dashboard-grid-item">6</div>
+        <div className="dashboard-grid-item">7</div>
+        <div className="dashboard-grid-item">8</div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
