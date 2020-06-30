0cf354a1dd446035c60107987e57cbadab730488
Faiz Qureshi <faizqureshi111@gmail.com>
2020-06-30 09:33:25 -0600

    installed redux and set up some basic reducers and actions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,18 @@ import App from "./App";
 
 // importing react router browser router
 import { BrowserRouter } from "react-router-dom";
+// importing redux provider
+import { Provider } from "react-redux";
+// importing redux store
+import store from "./redux/store";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
