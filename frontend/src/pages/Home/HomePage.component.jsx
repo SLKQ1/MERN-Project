2054e0da147fe5d11a57e1b1e20aceb237a7e92a
Faiz Qureshi <faizqureshi111@gmail.com>
2020-07-22 11:36:28 -0600

    refactored home component to use react useEffect hook to make api call.

--- frontend/src/pages/Home/HomePage.component.jsx
+++ frontend/src/pages/Home/HomePage.component.jsx
@@ -1,54 +1,61 @@
-import React, { Component } from "react";
+import React from "react";
 import "./HomePage.styles.css";
 import WristShotGrid from "../../components/WristShotGrid/WristShotGrid.component";
 import { connect } from "react-redux";
 import { fetchCollectionStartAsync } from "../../redux/wristShots/wristShots.action";
+import { useEffect } from "react";
 
-class HomePage extends Component {
-  componentDidMount() {
-    // calling async call to get wrist shots
-    const { fetchCollectionStartAsync } = this.props;
+/* TODO can potentially split this component up, 
+ making the api call in wrist shot grid this will avoid prop drilling 
+ which is currently happening
+*/
+
+function HomePage({
+  fetchCollectionStartAsync,
+  isFetching,
+  wrist_shots,
+  errorMessage,
+}) {
+  useEffect(() => {
     fetchCollectionStartAsync();
-  }
+  }, [fetchCollectionStartAsync]);
 
-  render() {
-    const { isFetching, wrist_shots, errorMessage } = this.props;
-    let homePageContent;
-    // content to be displayed if there is an error
-    if (errorMessage) {
-      homePageContent = (
-        <div className="wrist-shot-grid-container">
-          <h1>Ops something went wrong.. </h1>
-          <h3>{errorMessage}</h3>
-        </div>
-      );
-    }
-    // content to be displayed if fetching
-    else if (isFetching) {
-      homePageContent = (
-        <div className="wrist-shot-grid-container">
-          <h1>Loading...</h1>
-        </div>
-      );
-    }
-    // normal content displayed
-    else {
-      homePageContent = (
-        <div className="wrist-shot-grid-container">
-          <WristShotGrid wrist_shots={wrist_shots} />
-        </div>
-      );
-    }
-    return (
-      <div className="homepage">
-        <div className="homepage-title-content">
-          <h1>Welcome to Wrist Shot</h1>
-          <p>Post and explore amazing wrist shots.</p>
-        </div>
-        {homePageContent}
+  // const { isFetching, wrist_shots, errorMessage } = props;
+  let homePageContent;
+  // content to be displayed if there is an error
+  if (errorMessage) {
+    homePageContent = (
+      <div className="wrist-shot-grid-container">
+        <h1>Ops something went wrong.. </h1>
+        <h3>{errorMessage}</h3>
       </div>
     );
   }
+  // content to be displayed if fetching
+  else if (isFetching) {
+    homePageContent = (
+      <div className="wrist-shot-grid-container">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  // normal content displayed
+  else {
+    homePageContent = (
+      <div className="wrist-shot-grid-container">
+        <WristShotGrid wrist_shots={wrist_shots} />
+      </div>
+    );
+  }
+  return (
+    <div className="homepage">
+      <div className="homepage-title-content">
+        <h1>Welcome to Wrist Shot</h1>
+        <p>Post and explore amazing wrist shots.</p>
+      </div>
+      {homePageContent}
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => ({
